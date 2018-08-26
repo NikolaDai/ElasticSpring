ef4afc6b6257ecf23a4c73976f0a4d0d72380a0a
Daniel Dai <335860828@qq.com>
2018-08-26 21:37:51 +0800

    successful try to connect ElasticSearch 6.4 via Rest Client.

---
+++ src/test/java/com/w3dai/dao/elasticSearchTialTest.java
@@ -0,0 +1,9 @@
+package com.w3dai.dao;
+
+public class elasticSearchTialTest{
+    public static void main(String[] args) throws Exception{
+        elasticSearchTial X = new elasticSearchTial();
+
+        X.doQuery();
+    }
+}
