c2cf52fb7186e8925840b9844d10a01fa2c8160e
NicolaDai <335860828@qq.com>
2018-08-27 17:02:32 +0800

    try to use high level rest client to create indices.

---
+++ src/test/java/com/w3dai/dao/CreateIndexFromDataTest.java
@@ -0,0 +1,10 @@
+package com.w3dai.dao;
+
+import java.io.IOException;
+
+public class CreateIndexFromDataTest {
+    public static void main(String[] args) throws IOException {
+        CreateIndexFromData aCreateIndexFromData = new CreateIndexFromData();
+        aCreateIndexFromData.CreateIndex();
+    }
+}
