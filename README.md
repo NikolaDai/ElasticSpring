9bc434a389eb5c257132cedd154f4a96511eedaa
NicolaDai <335860828@qq.com>
2018-08-27 18:55:28 +0800

    successfully create the index using one of ways, binggo!

--- README.md
+++ README.md
@@ -5,3 +5,63 @@ The purpose of ElasticSpring is to implement elasticsearch functions using Sprin
 Preparations:
 In order to make everything run as expected, you need intall the softwares in the URL:http://www.cnblogs.com//hts-technology/p/8477258.html
 IK plugin installation:https://github.com/medcl/elasticsearch-analysis-ik
+
+
+about mapping:https://blog.csdn.net/napoay/article/details/73100110?locationNum=9&fps=1#324-termvector
+code of backup:
+
+ request.mapping("article", // <1>
+                "{\n" +
+                        "  \"article\": {\n" +
+                        "    \"properties\": {\n" +
+                        "      \"引题\": {\n" +
+                        "        \"type\": \"text\",\n" +
+                        "        \"store\":\"no\",\n"+
+                        "        \"term_analyzer\": \"with_positions_offsets\",\n" +
+                        "         \"analyzer\": \"ik_max_word\",\n" +
+                        "         \"search_analyzer\": \"ik_max_word\"\n" +
+                        "      },\n" +
+                        "      \"标题\": {\n" +
+                        "        \"type\": \"text\",\n" +
+                        "        \"store\":\"no\",\n"+
+                        "        \"term_analyzer\": \"with_positions_offsets\",\n" +
+                        "         \"analyzer\": \"ik_max_word\",\n" +
+                        "         \"search_analyzer\": \"ik_max_word\"\n" +
+                        "      },\n" +
+                        "      \"副题\": {\n" +
+                        "        \"type\": \"text\",\n" +
+                        "        \"store\":\"no\",\n"+
+                        "        \"term_analyzer\": \"with_positions_offsets\",\n" +
+                        "         \"analyzer\": \"ik_max_word\",\n" +
+                        "         \"search_analyzer\": \"ik_max_word\"\n" +
+                        "      },\n" +
+                        "      \"作者\": {\n" +
+                        "        \"type\": \"text\",\n" +
+                        "        \"index\":\"not_analyzed\"\n"+
+                        "      },\n" +
+                        "      \"责编\": {\n" +
+                        "        \"type\": \"text\",\n" +
+                        "        \"index\":\"not_analyzed\"\n"+
+                        "      },\n" +
+                        "      \"版面\": {\n" +
+                        "        \"type\": \"text\",\n" +
+                        "        \"index\":\"not_analyzed\"\n"+
+                        "      },\n" +
+                        "      \"分类\": {\n" +
+                        "        \"type\": \"text\",\n" +
+                        "        \"index\":\"not_analyzed\"\n"+
+                        "      },\n" +
+                        "      \"日期\": {\n" +
+                        "        \"type\": \"date\",\n" +
+                        "        \"format\":\"yyyymmdd\"\n"+
+                        "      },\n" +
+                        "      \"正文\": {\n" +
+                        "        \"type\": \"text\",\n" +
+                        "        \"term_analyzer\": \"with_positions_offsets\",\n" +
+                        "         \"analyzer\": \"ik_max_word\",\n" +
+                        "         \"search_analyzer\": \"ik_max_word\"\n" +
+                        "      }\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "}",
+                XContentType.JSON);
--- src/main/java/com/w3dai/dao/CreateIndexFromData.java
+++ src/main/java/com/w3dai/dao/CreateIndexFromData.java
@@ -4,22 +4,13 @@ package com.w3dai.dao;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
-import org.elasticsearch.action.ActionRequestBuilder;
-import org.elasticsearch.action.support.PlainActionFuture;
-import org.elasticsearch.client.IndicesAdminClient;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.util.HashMap;
-import java.util.Map;
 
 public class CreateIndexFromData{
     public void CreateIndex() throws IOException {
@@ -28,7 +19,7 @@ public class CreateIndexFromData{
                 RestClient.builder(
                         new HttpHost("localhost", 9200, "http")));
 
-        CreateIndexRequest request = new CreateIndexRequest("papers01");
+        CreateIndexRequest request = new CreateIndexRequest("papers04");
 
         request.settings(Settings.builder() // <1>
                 .put("index.number_of_shards", 3)
@@ -40,50 +31,50 @@ public class CreateIndexFromData{
                         "  \"article\": {\n" +
                         "    \"properties\": {\n" +
                         "      \"引题\": {\n" +
-                        "        \"type\": \"text\"\n" +
-                        "        \"store\":\"no\"\n"+
-                        "        \"term_analyzer\": \"with_positions_offsets\",\n" +
-                        "         \"index_analyzer\": \"ik_max_word\",\n" +
+                        "        \"type\": \"text\",\n" +
+                        "        \"store\":\"false\",\n"+
+                        "        \"term_vector\": \"with_positions_offsets\",\n" +
+                        "         \"analyzer\": \"ik_max_word\",\n" +
                         "         \"search_analyzer\": \"ik_max_word\"\n" +
                         "      },\n" +
                         "      \"标题\": {\n" +
-                        "        \"type\": \"text\"\n" +
-                        "        \"store\":\"no\"\n"+
-                        "        \"term_analyzer\": \"with_positions_offsets\",\n" +
-                        "         \"index_analyzer\": \"ik_max_word\",\n" +
+                        "        \"type\": \"text\",\n" +
+                        "        \"store\":\"false\",\n"+
+                        "        \"term_vector\": \"with_positions_offsets\",\n" +
+                        "         \"analyzer\": \"ik_max_word\",\n" +
                         "         \"search_analyzer\": \"ik_max_word\"\n" +
                         "      },\n" +
                         "      \"副题\": {\n" +
-                        "        \"type\": \"text\"\n" +
-                        "        \"store\":\"no\"\n"+
-                        "        \"term_analyzer\": \"with_positions_offsets\",\n" +
-                        "         \"index_analyzer\": \"ik_max_word\",\n" +
+                        "        \"type\": \"text\",\n" +
+                        "        \"store\":\"false\",\n"+
+                        "        \"term_vector\": \"with_positions_offsets\",\n" +
+                        "         \"analyzer\": \"ik_max_word\",\n" +
                         "         \"search_analyzer\": \"ik_max_word\"\n" +
                         "      },\n" +
                         "      \"作者\": {\n" +
-                        "        \"type\": \"text\"\n" +
-                        "        \"index\":\"not_analyzed\"\n"+
+                        "        \"type\": \"text\",\n" +
+                        "        \"index\":\"false\"\n"+
                         "      },\n" +
                         "      \"责编\": {\n" +
-                        "        \"type\": \"text\"\n" +
-                        "        \"index\":\"not_analyzed\"\n"+
+                        "        \"type\": \"text\",\n" +
+                        "        \"index\":\"false\"\n"+
                         "      },\n" +
                         "      \"版面\": {\n" +
-                        "        \"type\": \"text\"\n" +
-                        "        \"index\":\"not_analyzed\"\n"+
+                        "        \"type\": \"text\",\n" +
+                        "        \"index\":\"false\"\n"+
                         "      },\n" +
                         "      \"分类\": {\n" +
-                        "        \"type\": \"text\"\n" +
-                        "        \"index\":\"not_analyzed\"\n"+
+                        "        \"type\": \"text\",\n" +
+                        "        \"index\":\"false\"\n"+
                         "      },\n" +
                         "      \"日期\": {\n" +
-                        "        \"type\": \"date\"\n" +
+                        "        \"type\": \"date\",\n" +
                         "        \"format\":\"yyyymmdd\"\n"+
                         "      },\n" +
                         "      \"正文\": {\n" +
-                        "        \"type\": \"text\"\n" +
-                        "        \"term_analyzer\": \"with_positions_offsets\",\n" +
-                        "         \"index_analyzer\": \"ik_max_word\",\n" +
+                        "        \"type\": \"text\",\n" +
+                        "        \"term_vector\": \"with_positions_offsets\",\n" +
+                        "         \"analyzer\": \"ik_max_word\",\n" +
                         "         \"search_analyzer\": \"ik_max_word\"\n" +
                         "      }\n" +
                         "    }\n" +
@@ -98,28 +89,5 @@ public class CreateIndexFromData{
 
         System.out.println(acknowledged + "##" + shardsAcknowledged);
 
-
-        /**
-        TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
-                .addTransportAddress(new TransportAddress(InetAddress.getByName("localhost"), 9300));
-        IndicesAdminClient indicesAdminClient = client.admin().indices();
-
-        Map<String, Object> typesName = new HashMap<String, Object>();
-        typesName.put("引题", "type=text");
-        typesName.put("正题", "type=text");
-        typesName.put("副题", "type=text");
-        typesName.put("作者", "type=text");
-        typesName.put("日期", "type=text");
-        typesName.put("版面", "type=text");
-        typesName.put("分类", "type=text");
-        typesName.put("体裁", "type=text");
-
-        indicesAdminClient.prepareCreate("paper10")
-                .setSettings(Settings.builder()
-                        .put("index.number_of_shards", 3)
-                        .put("index.number_of_replicas", 2)
-                ).addMapping("article", typesName)
-                .get();
-         **/
     }
 }
