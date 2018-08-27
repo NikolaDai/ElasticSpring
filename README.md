0ed97fc482e896db0192d84907cc5ef5593f17d2
NicolaDai <335860828@qq.com>
2018-08-27 16:59:42 +0800

    try to use high level rest client to create indices.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ElasticSpring
 The purpose of ElasticSpring is to implement elasticsearch functions using Spring. 
+
+
+Preparations:
+In order to make everything run as expected, you need intall the softwares in the URL:http://www.cnblogs.com//hts-technology/p/8477258.html
+IK plugin installation:https://github.com/medcl/elasticsearch-analysis-ik
--- src/main/java/com/w3dai/dao/CreateIndexFromData.java
+++ src/main/java/com/w3dai/dao/CreateIndexFromData.java
@@ -1,19 +1,105 @@
 package com.w3dai.dao;
 
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.ActionRequestBuilder;
+import org.elasticsearch.action.support.PlainActionFuture;
 import org.elasticsearch.client.IndicesAdminClient;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CreateIndexFromData{
-    public void IndexGenerator() throws Exception{
+    public void CreateIndex() throws IOException {
+        //create the high level client of the localhost
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http")));
 
+        CreateIndexRequest request = new CreateIndexRequest("papers01");
+
+        request.settings(Settings.builder() // <1>
+                .put("index.number_of_shards", 3)
+                .put("index.number_of_replicas", 2)
+        );
+
+        request.mapping("article", // <1>
+                "{\n" +
+                        "  \"article\": {\n" +
+                        "    \"properties\": {\n" +
+                        "      \"引题\": {\n" +
+                        "        \"type\": \"text\"\n" +
+                        "        \"store\":\"no\"\n"+
+                        "        \"term_analyzer\": \"with_positions_offsets\",\n" +
+                        "         \"index_analyzer\": \"ik_max_word\",\n" +
+                        "         \"search_analyzer\": \"ik_max_word\"\n" +
+                        "      },\n" +
+                        "      \"标题\": {\n" +
+                        "        \"type\": \"text\"\n" +
+                        "        \"store\":\"no\"\n"+
+                        "        \"term_analyzer\": \"with_positions_offsets\",\n" +
+                        "         \"index_analyzer\": \"ik_max_word\",\n" +
+                        "         \"search_analyzer\": \"ik_max_word\"\n" +
+                        "      },\n" +
+                        "      \"副题\": {\n" +
+                        "        \"type\": \"text\"\n" +
+                        "        \"store\":\"no\"\n"+
+                        "        \"term_analyzer\": \"with_positions_offsets\",\n" +
+                        "         \"index_analyzer\": \"ik_max_word\",\n" +
+                        "         \"search_analyzer\": \"ik_max_word\"\n" +
+                        "      },\n" +
+                        "      \"作者\": {\n" +
+                        "        \"type\": \"text\"\n" +
+                        "        \"index\":\"not_analyzed\"\n"+
+                        "      },\n" +
+                        "      \"责编\": {\n" +
+                        "        \"type\": \"text\"\n" +
+                        "        \"index\":\"not_analyzed\"\n"+
+                        "      },\n" +
+                        "      \"版面\": {\n" +
+                        "        \"type\": \"text\"\n" +
+                        "        \"index\":\"not_analyzed\"\n"+
+                        "      },\n" +
+                        "      \"分类\": {\n" +
+                        "        \"type\": \"text\"\n" +
+                        "        \"index\":\"not_analyzed\"\n"+
+                        "      },\n" +
+                        "      \"日期\": {\n" +
+                        "        \"type\": \"date\"\n" +
+                        "        \"format\":\"yyyymmdd\"\n"+
+                        "      },\n" +
+                        "      \"正文\": {\n" +
+                        "        \"type\": \"text\"\n" +
+                        "        \"term_analyzer\": \"with_positions_offsets\",\n" +
+                        "         \"index_analyzer\": \"ik_max_word\",\n" +
+                        "         \"search_analyzer\": \"ik_max_word\"\n" +
+                        "      }\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "}",
+                XContentType.JSON);
+
+        CreateIndexResponse createIndexResponse = client.indices().create(request, RequestOptions.DEFAULT);
+
+        boolean acknowledged = createIndexResponse.isAcknowledged();
+        boolean shardsAcknowledged = createIndexResponse.isShardsAcknowledged();
+
+        System.out.println(acknowledged + "##" + shardsAcknowledged);
+
+
+        /**
         TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
                 .addTransportAddress(new TransportAddress(InetAddress.getByName("localhost"), 9300));
         IndicesAdminClient indicesAdminClient = client.admin().indices();
@@ -34,5 +120,6 @@ public class CreateIndexFromData{
                         .put("index.number_of_replicas", 2)
                 ).addMapping("article", typesName)
                 .get();
+         **/
     }
 }
